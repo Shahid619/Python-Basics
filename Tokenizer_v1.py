e5cf3eeb734d3438ac24fdae1eae423d7e12ad9a
Shahid khan <143776984+Shahid619@users.noreply.github.com>
2025-11-26 10:24:20 +0500

    Manual Tokenizer V-1
    this is the starting of creating Tokenizer manually.

---
+++ Tokenizer_v1.py
@@ -0,0 +1,18 @@
+# Tokenizer v1 → split on spaces only (manual, don’t use .split())
+# print('this is phase 1 of creationg of Tokenizer manually!')
+
+'''
+    input text 
+        chck for whitespace 
+            if : is there -> replace with -> comma.
+            else : continue.
+'''
+import re
+def splitt(text):
+    text=re.sub(r' ',',',text)   
+    return text
+
+text='this is phase 1 of creationg of Tokenizer manually'
+print(splitt(text))
+
+# Now here we used : re.sub .....let's create this sub.  TO know how exactly he is doing this .
