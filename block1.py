eec5845adb4698aaa69c4a4e5f16fe71703a5c37
Shahid khan <143776984+Shahid619@users.noreply.github.com>
2025-11-28 08:19:38 +0500

    Basic practice of string function
    Take user input for name, age → print formatted output.
    
    Convert temperature (C → F, F → C).
    
    Extract first/last name from a full name string.
    
    Validate if an input is numeric or not.

---
+++ block1.py
@@ -0,0 +1,46 @@
+print('THis is whole block 1 problems file')
+
+# ==========================================================
+# Take user input for name, age → print formatted output.
+
+# Name=input('Enter your name: ')
+# age = int(input('Enter your age: '))
+
+# print(F"My name is  {Name} and I am {age} years old.")
+
+
+# ====================================================
+# Convert temperature (C → F, F → C).
+
+# temp = int(input('Enter temperature: '))
+# choice =int(input('1- Change into celcius  , 2- Change into Farenheit: '))
+# if choice==1:
+#     print(f'{temp} Farenheit == {temp+32} Celcius')
+# elif choice==2:
+#     print(f'{temp} Ceclius == {temp-32} Farenheit')
+# else:
+#     print('Invalid choice.')
+
+
+# ======================================================
+# Extract first/last name from a full name string.
+
+
+text='Shahid khan'
+
+# print(f'first name : {text[:6]}')
+# print(f'last name : {text[7:]}')
+
+
+# =======================================================
+# Validate if an input is numeric or not.
+
+inp = '12a'
+# if inp.isdigit():
+#     print(True)
+# else:
+#     print(False)
+
+
+# ======================================================
+
